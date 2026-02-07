70cd782a7a89cb97661d028d53b2eaa46f12ebe3
khadijaceurrouj57-coder <khadijaceurrouj57@gmail.com>
2026-02-07 21:41:40 +0100

    Add client address form in traitement.php

---
+++ prototype/traitement.php
@@ -0,0 +1,11 @@
+<form action="traitement.php" method="POST">
+    <fieldset>
+        <legend>Adresse client</legend>
+        <label>Nom :</label> <input type="text" name="nom" required><br>
+        <label>Prénom :</label> <input type="text" name="prenom" required><br>
+        <label>Adresse :</label> <input type="text" name="adresse" required><br>
+        <label>Ville :</label> <input type="text" name="ville" required><br>
+        <label>Code Postal :</label> <input type="text" name="cp" required><br>
+        <button type="submit">Envoyer</button>
+    </fieldset>
+</form>
